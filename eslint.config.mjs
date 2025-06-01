b7e47eed172ccca2364058d5e8ba63652a3581cc
okha486@gmail.com <okha486@gmail.com>
2025-06-01 10:38:09 +0900

    프로젝트 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import node from '@oka-app-ui/eslint-config/node'
+import typescript from '@oka-app-ui/eslint-config/typescript'
+
+export default [
+    {
+        ignores: ['**/dist/**'],
+    },
+    ...node,
+    ...typescript,
+    {
+        rules: {
+            'unicorn/prefer-module': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            'sonarjs/no-nested-conditional': 'off',
+        },
+    },
+]
